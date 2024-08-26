64f9656861e01ca5e2c3acd0c15bc04ee5db7153
TJSidhu1 <124082232+TJSidhu1@users.noreply.github.com>
2024-08-26 02:06:37 +0000

    variables file changed

---
+++ 01_basics/variables.js
@@ -0,0 +1,12 @@
+const id = 1
+let studentName = "Tamanjeet"
+var schoolName = "Chico State"
+courseName = "Computer Science"
+
+console.table([id, studentName, schoolName, courseName])
+
+// id = 2
+studentName = "Tamanjeet Kaur Sidhu"
+schoolName = "California State University, Chico"
+courseName = "MS CS"
+console.table([id, studentName, schoolName, courseName])
