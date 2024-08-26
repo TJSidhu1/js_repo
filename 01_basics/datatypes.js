f84b2b06a97ec1900b5d11c91d712728c8d8efe4
TJSidhu1 <124082232+TJSidhu1@users.noreply.github.com>
2024-08-26 02:45:55 +0000

    datatypes changes cmmit

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,9 @@
+const id = 1
+const Name = "Taman"
+let studentName = "Tamanjeet"
+let password = true
+let pwd2
+let pwd3 = null
+
+console.log(typeof id)
+console.table([typeof id, typeof Name, typeof studentName, typeof password, typeof pwd2, typeof pwd3])
