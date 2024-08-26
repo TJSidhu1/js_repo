3c71e7729099722c5384934015a54b64d84ecb43
TJSidhu1 <124082232+TJSidhu1@users.noreply.github.com>
2024-08-26 06:40:02 +0000

    Conversion operation commit

---
+++ 01_basics/conversions.js
@@ -0,0 +1,18 @@
+// let score = 33
+// console.log(typeof score)
+
+// let scorestring = String(score)
+// console.log(typeof scorestring)
+// console.log(scorestring)
+
+// let scorebool = Boolean(score)
+// console.log(scorebool)
+// console.log(typeof scorebool)
+
+let score 
+console.log(typeof score)
+console.log(score)
+
+let scorestring = String(score)
+console.log(typeof scorestring)
+console.log(scorestring)
