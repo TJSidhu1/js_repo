1e7c6542ed0d166de6837bfc0e85ebf675af741a
TJSidhu1 <124082232+TJSidhu1@users.noreply.github.com>
2024-08-26 02:21:29 +0000

    Added comments to variables.js

--- 01_basics/variables.js
+++ 01_basics/variables.js
@@ -10,3 +10,7 @@ studentName = "Tamanjeet Kaur Sidhu"
 schoolName = "California State University, Chico"
 courseName = "MS CS"
 console.table([id, studentName, schoolName, courseName])
+
+/* Prefer not to use var because of issue in block scope
+   and functional scope.
+*/
