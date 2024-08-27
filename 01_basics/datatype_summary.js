3d480c912f8033c9e7ed81d41be9a4488854c8bd
TJSidhu1 <124082232+TJSidhu1@users.noreply.github.com>
2024-08-27 22:30:59 +0000

    Datatypes summary commit

---
+++ 01_basics/datatype_summary.js
@@ -0,0 +1,16 @@
+/*
+JavaScript is a dynamically typed language, which means that data types of variables are 
+determined by the value they hold at runtime and can change throughout the program as 
+we assign different values to them.
+
+There are two categories of data types:
+
+1)Primitive: These are call-by-value. The types of primitive data types include String, Number, Boolean, Null,
+  Undefined, Symbol, and BigInt. Note that Symbols uniquely identify variables, even if two variables have the 
+  same data and data type. Primitive variables datatype is same but null datatype is object 
+
+2) Non-primitive: These are call-by-reference. The types of non-primitive data types are Array, Object, and Function.
+   The data type for these is 'object,' but for functions, they return a data type of 'function,' though they also have
+   an object data type.
+*/
+
