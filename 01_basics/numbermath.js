fe4111fb23ff70ba3d232b7b4e9a6ba99f62c559
TJSidhu1 <124082232+TJSidhu1@users.noreply.github.com>
2024-08-30 02:18:27 +0000

    more changes to the math library functions

--- 01_basics/numbermath.js
+++ 01_basics/numbermath.js
@@ -1,4 +1,13 @@
 let num = 15
 console.log(typeof num)
-let num2 = new Number(11)
+let num2 = new Number(110)
 console.log(num2)
+
+console.log(num2.toString().length)
+
+// Math library
+console.log(Math)
+console.log(Math.abs(-10))
+console.log(Math.round(4.6))
+console.log((Math.random()*10).toFixed(2))
+console.log((Math.random()*10))
