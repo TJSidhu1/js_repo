49fed699d7098909ef02dbdcff86134741493004
TJSidhu1 <124082232+TJSidhu1@users.noreply.github.com>
2024-09-01 08:49:28 +0000

    array changes

--- 02_basics/arrays.js
+++ 02_basics/arrays.js
@@ -6,3 +6,13 @@ arr3 = new Array(5,4,3,2,1)
 console.log(arr1)
 console.log(arr2)
 console.log(arr3)
+
+arr1.push("a")
+console.log(arr1)
+arr2.pop()
+console.log(arr2)
+
+const array1 = arr1.slice(1,3)
+console.log(array1)
+const array2 = arr1.splice(1,3)
+console.log(array2)
