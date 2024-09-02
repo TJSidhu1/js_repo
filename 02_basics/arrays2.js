11983370af41602851e885fcf5b67f93b1890ac6
TJSidhu1 <124082232+TJSidhu1@users.noreply.github.com>
2024-09-02 08:11:30 +0000

    Modifications

--- 02_basics/arrays2.js
+++ 02_basics/arrays2.js
@@ -1,5 +1,8 @@
 const numbers = [1,2,3]
 const alphanumeric = ['one', 'two', 'three']
 
-numbers.push(alphanumeric)
-console.log(numbers)                   //array can be pushed to another array resulting in array within an array
+// numbers.push(alphanumeric)
+// console.log(numbers)                   //array can be pushed to another array resulting in array within an array
+
+const num3 = numbers.concat(alphanumeric)
+console.log(num3)
