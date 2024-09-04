8f8d6990e4ed214e82759babeca4f279add7584a
TJSidhu1 <124082232+TJSidhu1@users.noreply.github.com>
2024-09-04 15:05:44 +0000

    More changes

--- 02_basics/objects.js
+++ 02_basics/objects.js
@@ -1,10 +1,23 @@
+const mySymbol = Symbol("key1")
+
 const obj = {
     name : "Tamanjeet",
     id : 12,
     location: "Sacramento",
     Email: "tkaur",
+    [mySymbol] : "key1",
     isloggedin : false
 }
 
 console.log(obj.name)             //access using dot operator
 console.log(obj["name"])          //access using square brackets
+// console.log(obj.mySymbol)
+// console.log(typeof obj.mySymbol)
+console.log(obj[mySymbol])
+console.log(typeof obj[mySymbol])
+
+obj.name = "TJ Sidhu"
+console.log(obj)
+Object.freeze(obj)
+obj.name = "Tamanjeet Kaur"
+console.log(obj)
