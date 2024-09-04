7b6e375b3956cf6369a21e02394f1826876b9b1d
TJSidhu1 <124082232+TJSidhu1@users.noreply.github.com>
2024-09-04 02:53:26 +0000

    arrays2 changes

--- 02_basics/arrays2.js
+++ 02_basics/arrays2.js
@@ -4,8 +4,12 @@ const alphanumeric = ['one', 'two', 'three']
 // numbers.push(alphanumeric)
 // console.log(numbers)                   //array can be pushed to another array resulting in array within an array
 
-const num3 = numbers.concat(alphanumeric)
-console.log(num3)
+// const num3 = numbers.concat(alphanumeric)
+// console.log(num3)
 
-const num4 = [...numbers, ...alphanumeric]       //is called spread function and can be used to merge as many arraays as possible
-console.log(num4)
+// const num4 = [...numbers, ...alphanumeric]       //is called spread function and can be used to merge as many arraays as possible
+// console.log(num4)
+
+const num5 = [1,2,3, [4,5,[6,7]]]
+const num6 = num5.flat(3)
+console.log(num6)
