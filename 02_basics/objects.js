5d907b87cfdf858f19e7e3cc64fe44ecde0127d3
TJSidhu1 <124082232+TJSidhu1@users.noreply.github.com>
2024-09-04 14:55:17 +0000

    Objects in js

---
+++ 02_basics/objects.js
@@ -0,0 +1,10 @@
+const obj = {
+    name : "Tamanjeet",
+    id : 12,
+    location: "Sacramento",
+    Email: "tkaur",
+    isloggedin : false
+}
+
+console.log(obj.name)             //access using dot operator
+console.log(obj["name"])          //access using square brackets
