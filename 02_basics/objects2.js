88cb381c904a3670720cffd7af34ed10569a9423
TJSidhu1 <124082232+TJSidhu1@users.noreply.github.com>
2024-09-06 15:06:18 +0000

    More changes to objects2

--- 02_basics/objects2.js
+++ 02_basics/objects2.js
@@ -2,7 +2,14 @@ const user = new Object()      //singleton object
 appuser = {
     name : "Tamanjeet",
     id : 123,
-    isloggedin : true
+    isloggedin : true,
+    details : {
+        streetAddress : 657,
+        city : "NY",
+        zip : 23465
+    }
+
 }
 
 console.log(appuser.id)
+console.log(appuser.details.zip)              //object within an object
