a05c42fdafba11d654793f7f7bf07fade8bea57c
TJSidhu1 <124082232+TJSidhu1@users.noreply.github.com>
2024-09-10 05:22:26 +0000

    More updations

--- 03_basics/scope.js
+++ 03_basics/scope.js
@@ -1,10 +1,19 @@
 let a = 10
 console.log(a)
 
-while(a = 10)
+while(a == 10)
 {
     a = 12
     console.log(a)
+    let b = 14 
+    // With let: The variable b is limited to the scope in which it is declared (inside the loop). Once you leave the loop,
+    // b is no longer accessible.
+    // Without let: The variable b becomes global, meaning it can be accessed outside the loop and throughout the rest
+    //  of your code.
+    
+
+    console.log(b)
 }
 
 console.log(a)
+console.log(b)
